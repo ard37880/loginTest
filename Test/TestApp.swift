d703009ed71f23acb53af8f1f40571dce4f447db
Andrew Davis <andrew.davis37880@gmail.com>
2022-03-28 21:09:53 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Andrew Davis on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
